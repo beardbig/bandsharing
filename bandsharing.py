24b16d317d79ec89387fe11bceceff3dd2a1b804
beardbig <damianobarboni@gmail.com>
2014-06-11 23:53:25 +0200

    read file ok

--- bandsharing.py
+++ bandsharing.py
@@ -9,6 +9,7 @@
 #               
 #:::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+import traceback
 
 class BandSharing():
     def __init__(self):
@@ -17,22 +18,49 @@ class BandSharing():
         
         self.females = {}
         self.males   = {}
-        
+    
+    #:::::::::::::::::::::::::
+    #      read dataset    
+    #:::::::::::::::::::::::::
     def read(self):
         f = open( self.input_file, "r" )
         while True:
             line=f.readline()
             if not line: break
-            print line.strip()
+            self.parseLine( line )
         f.close()
+        self.females[16].sort()
+        print self.females[16]
         
-        
-        
-        None
+    def parseLine( self, line ):
+        try:
+            cells = line.strip().split(",")
+            gender = self.getGender( cells[ -1 ] )
+            key = int( cells[ 0 ], 10 )
+            if not key in gender: gender[ key ] = [] 
+            gender[ key ].append( cells[ 2 ] + "-" + cells[ 1 ]  )
+            gender[ key ].append( cells[ 3 ] + "-" + cells[ 1 ]  )
+            gender[ key ].append( cells[ 4 ] + "-" + cells[ 1 ]  )
+            gender[ key ].append( cells[ 5 ] + "-" + cells[ 1 ]  )
+        except:
+            print traceback.format_exc()
+            #line doesn'tcontain valid data
+            pass
+
+    def getGender( self, cell ):
+        print cell.lower()
+        if cell.lower() == "m":
+            gender = self.males
+        elif cell.lower() == "f":
+            gender = self.females
+        else:
+            gender = None
+        return gender       
+
+   
     def write(self):
         None
-    
-    def run(self):
+    def run( self ):
         self.read()
         
 BandSharing().run()
