94e44aa1eeec306fa1f6af47dde3cb6e3f387af2
beardbig <damianobarboni@gmail.com>
2014-06-11 18:10:41 +0200

    read data from file

--- bandsharing.py
+++ bandsharing.py
@@ -8,3 +8,31 @@
 #               First test version     
 #               
 #:::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+class BandSharing():
+    def __init__(self):
+        self.input_file = "tests/test_data/test.csv"
+        self.output_file = "out.csv"
+        
+        self.females = {}
+        self.males   = {}
+        
+    def read(self):
+        f = open( self.input_file, "r" )
+        while True:
+            line=f.readline()
+            if not line: break
+            print line.strip()
+        f.close()
+        
+        
+        
+        None
+    def write(self):
+        None
+    
+    def run(self):
+        self.read()
+        
+BandSharing().run()
