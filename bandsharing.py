4531f7aa1f0ec7ff8b094c1c378be9d01e6a230e
beardbig <damianobarboni@gmail.com>
2014-06-11 12:19:40 +0200

    Repo initialized. Upload original TDS

---
+++ bandsharing.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+#:::::::::::::::::::::::::::::::::::::::::::::::::::::
+#Author:        Damiano Barboni <damianobarboni@gmail.com>
+#Version:       0.1
+#Description:   Script used to calculate Band Sharing between individuals from the same lines
+#Changelog:     Wed Jun 11 12:07:33 CEST 2014
+#               First test version     
+#               
+#:::::::::::::::::::::::::::::::::::::::::::::::::::::
---
+++ tests/bandsharing_tests.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+#:::::::::::::::::::::::::::::::::::::::::::::::::::::
+#Author:        Damiano Barboni <damianobarboni@gmail.com>
+#Version:       0.1
+#Description:   Script used to test bandsharing.py
+#Changelog:     Wed Jun 11 12:07:33 CEST 2014
+#               First test version     
+#               
+#:::::::::::::::::::::::::::::::::::::::::::::::::::::
